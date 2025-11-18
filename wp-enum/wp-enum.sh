ff2b3514d7bc20c44dafc3a2998d3663755231c3
Nazaret <nazaretcj09@gmail.com>
2025-11-18 14:33:35 +0100

    Update wp-enum.sh

--- wp-enum/wp-enum.sh
+++ wp-enum/wp-enum.sh
@@ -1,4 +1,3 @@
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 #!/bin/bash
 
 # WordPress Enumeration Script - Professional Edition
